d62686f36b663268945cbc939f7e6768338138ff
eoin <eoind@vey.ie>
2020-08-09 01:13:36 +0100

    Fix indenting of .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  rules: {
-      eqeqeq: ["error", "always"]
-  },
-  overrides: [
-      {
-          // Disabling non-null assertions checks in tests because we are asserting
-          // them to be non-null with jest, the type system is just not aware of
-          // that
-          files: ["**/*.test.ts"],
-          rules: {
-              "@typescript-eslint/no-non-null-assertion": "off"
-          }
-      }
-  ]
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    rules: {
+        eqeqeq: ["error", "always"]
+    },
+    overrides: [
+        {
+            // Disabling non-null assertions checks in tests because we are asserting
+            // them to be non-null with jest, the type system is just not aware of
+            // that
+            files: ["**/*.test.ts"],
+            rules: {
+                "@typescript-eslint/no-non-null-assertion": "off"
+            }
+        }
+    ]
 };
